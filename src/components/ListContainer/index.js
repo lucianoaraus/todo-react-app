2f1ba0714d0d28def180ad069688ee0b4d14e5d4
lucianoaraus <lucianoaraus@gmail.com>
2022-06-13 13:17:57 -0300

    filter function finished

--- src/components/ListContainer/index.js
+++ src/components/ListContainer/index.js
@@ -11,19 +11,45 @@ const filterOptions = ["All", "Completed", "Uncompleted"];
 
 function ListContainer() {
   const { tasks, resetTasks } = useContext(ListContext);
-  const [filter, setFilter] = useState(filterOptions[0]);
+  const [filterCase, setFilterCase] = useState(filterOptions[0]);
 
   const handleFilter = (event) => {
     const value = event.target.value;
-    setFilter(value);
+    setFilterCase(value);
+  };
+
+  const FILTER_CASES = {
+    All: tasks
+      .filter((t) => !t.deleted)
+      .map((task) => {
+        if (!task.completed) {
+          return <Item data={task} key={task.id} />;
+        } else {
+          return <RealizedItem data={task} key={task.id} />;
+        }
+      }),
+    Completed: tasks
+      .filter((t) => !t.deleted && t.completed)
+      .map((task) => {
+        return <RealizedItem data={task} key={task.id} />;
+      }),
+    Uncompleted: tasks
+      .filter((t) => !t.deleted && !t.completed)
+      .map((task) => {
+        return <Item data={task} key={task.id} />;
+      }),
   };
 
   return (
     <div className="list-container">
       <NewItem />
 
-      {/* Filter Options */}
-      <select id="list" onChange={(e) => handleFilter(e)}>
+      {/* Filter Options Selector */}
+      <select
+        id="list"
+        className="select-input"
+        onChange={(e) => handleFilter(e)}
+      >
         {filterOptions.map((p, i) => {
           return (
             <option value={p} key={i}>
@@ -33,15 +59,8 @@ function ListContainer() {
         })}
       </select>
 
-      {tasks
-        .filter((t) => !t.deleted)
-        .map((task) => {
-          if (!task.completed) {
-            return <Item data={task} key={task.id} />;
-          } else {
-            return <RealizedItem data={task} key={task.id} />;
-          }
-        })}
+      {/* Filtered Options */}
+      {FILTER_CASES[filterCase]}
 
       <button className="reset-button" onClick={() => resetTasks()}>
         reset progress
